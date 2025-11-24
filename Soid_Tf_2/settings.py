3d757352878135ce1aaa8063a9c001f08f8b4706
jhonf11985 <jhonf1_b@hotmail.com>
2025-11-24 10:38:34 -0400

    Agregar Whitenoise y configurar staticfiles para Render

--- Soid_Tf_2/settings.py
+++ Soid_Tf_2/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <<--- AÑADIR ESTA LÍNEA
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,6 +122,7 @@ STATIC_URL = 'static/'
 
 # Render necesita esto para servir archivos estáticos
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Media (fotos y archivos del usuario)
