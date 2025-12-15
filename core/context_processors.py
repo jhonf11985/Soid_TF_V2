b33af4e6f413b8a6002641ccfa17100e6ae828e2
jhonf11985 <jhonf1_b@hotmail.com>
2025-12-15 13:05:37 -0400

    1

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,10 +1,18 @@
-from .models import ConfiguracionSistema
+from .models import ConfiguracionSistema, Module
 
 def configuracion_global(request):
     """
-    Envía la configuración del sistema a TODAS las plantillas.
+    Envía la configuración del sistema y los módulos activos
+    a TODAS las plantillas.
     """
     config = ConfiguracionSistema.load()
+
+    modulos_activos = {
+        m.code: m
+        for m in Module.objects.filter(is_enabled=True)
+    }
+
     return {
-        "CFG": config
+        "CFG": config,
+        "MODULOS_ACTIVOS": modulos_activos,
     }
